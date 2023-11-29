d8e586e53b1eaccb8c8df9b3608e4ab582bf7e44
webcaique <sidraocaique@gmail.com>
2023-11-29 14:36:47 -0300

    Commit

--- Stein/SteinProject/src/pages/FaqScreen/index.js
+++ Stein/SteinProject/src/pages/FaqScreen/index.js
@@ -1,107 +1,207 @@
-import React from "react";
-import {View, Text, ScrollView, TouchableOpacity, Image} from "react-native";
-import styles from "./style"
+import React, {useState} from 'react';
+import {View, Text, ScrollView, TouchableOpacity, Image} from 'react-native';
+import styles from './style';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 const FaqScreen = () => {
-    return(
-        <View style={styles.container}>
-            <ScrollView>
-                <View style={styles.faqText}>
-                    <Text style={{fontSize:100, color:"#000000"}}>FAQ</Text>
-                </View>
-                <View style={styles.spaceBetweenBoxes}>
-                    <View style={styles.boxes}>
-                        <Text  style={styles.txt}>Quem somos?</Text>
-                        <TouchableOpacity style={styles}>
-                            <Image 
-                            source={
-                                {uri:"https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48"}
-                                }
-                            style={styles.imagem}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.lines}/>
-                </View>
+  const [texto1, setTexto1] = useState(false);
+  const [texto2, setTexto2] = useState(false);
+  const [texto3, setTexto3] = useState(false);
+  const [texto4, setTexto4] = useState(false);
 
-                <View style={styles.spaceBetweenBoxes}>
-                    <View style={styles.boxes}>
-                        <Text style={styles.txt}>Como ganhar Pontos EV</Text>
-                        <TouchableOpacity style={styles}>
-                            <Image 
-                            source={
-                                {uri:"https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48"}
-                                }
-                            style={styles.imagem}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.lines}/>
-                </View>
-
-                <View style={styles.spaceBetweenBoxes}>
-                    <View style={styles.boxes}>
-                        <Text style={styles.txt}>Contato</Text>
-                        <TouchableOpacity style={styles}>
-                            <Image 
-                            source={
-                                {uri:"https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48"}
-                                }
-                            style={styles.imagem}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.lines}/>
-                </View>
-
-                <View style={styles.spaceBetweenBoxes}>
-                    <View style={styles.boxes}>
-                        <Text style={styles.txt}>Quais são os benéficios do Stein</Text>
-                        <TouchableOpacity style={styles}>
-                            <Image 
-                            source={
-                                {uri:"https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48"}
-                                }
-                            style={styles.imagem}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.lines}/>
-                </View>
-
-                <View style={styles.spaceBetweenBoxes}>
-                    <View style={styles.boxes}>
-                        <Text style={styles.txt}>Como funcionam os pontos Ev</Text>
-                        <TouchableOpacity style={styles}>
-                            <Image 
-                            source={
-                                {uri:"https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48"}
-                                }
-                            style={styles.imagem}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.lines}/>
-                </View>
-
-                <View style={styles.spaceBetweenBoxes}>
-                    <View style={styles.boxes}>
-                        <Text style={styles.txt}>Como mudar minha senha</Text>
-                        <TouchableOpacity style={styles}>
-                            <Image 
-                            source={
-                                {uri:"https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48"}
-                                }
-                            style={styles.imagem}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.lines}/>
-                </View>
-                
-            </ScrollView>
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.faqText}>
+          <Text style={{fontSize: RFValue(100), color: '#000000'}}>FAQ</Text>
         </View>
-    )
-}
+
+        <View style={styles.spaceBetweenBoxes}>
+          <View style={styles.boxes}>
+            <Text style={styles.txt}>Quem Somos?</Text>
+            <TouchableOpacity
+              style={styles}
+              onPress={() => {
+                setTexto1(!texto1);
+              }}>
+              <Image
+                source={{
+                  uri: 'https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48',
+                }}
+                style={{
+                  ...styles.imagem,
+                  transform: [{rotate: `${90 + (texto1 ? 180 : 0)}deg`}],
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.lines} />
+          {texto1 ? (
+            <View style={styles.boxTexto1}>
+              <Text style={styles.texto1}>
+                A Stein foi fundada por um grupo de cinco estudantes durante o
+                processo de realização de seus trabalhos de conclusão de curso
+                (TCC). Eles compartilharam uma visão em comum e transformaram
+                essa visão em realidade, dando início à Stein como uma
+                iniciativa que cresceu a partir do empenho e dedicação dos
+                fundadores.
+              </Text>
+            </View>
+          ) : (
+            console.log('HI')
+          )}
+        </View>
+
+        <View style={styles.spaceBetweenBoxes}>
+          <View style={styles.boxes}>
+            <Text style={styles.txt}>Quais são os benéficios do Stein?</Text>
+            <TouchableOpacity
+              style={styles}
+              onPress={() => {
+                setTexto2(!texto2);
+              }}>
+              <Image
+                source={{
+                  uri: 'https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48',
+                }}
+                style={{
+                  ...styles.imagem,
+                  transform: [{rotate: `${90 + (texto2 ? 180 : 0)}deg`}],
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.lines} />
+          {texto2 ? (
+            <View style={styles.boxTexto1}>
+              <View style={styles.boxTexto2}>
+                <View style={styles.spot} />
+                <Text style={styles.texto2}>
+                  Você se beneficiará de produtos e serviços que são projetados
+                  com uma mentalidade fresca e uma abordagem inovadora. Nossa
+                  equipe está sempre pensando fora da caixa para oferecer
+                  soluções que atendam às suas necessidades de maneira única.
+                </Text>
+              </View>
+              <View style={styles.boxTexto2}>
+                <View style={styles.spot} />
+                <Text style={styles.texto2}>
+                  Acreditamos na constante melhoria e evolução, o que significa
+                  que nossos produtos e serviços são atualizados regularmente
+                  com base nas mais recentes descobertas e melhores práticas.
+                </Text>
+              </View>
+              <View style={{...styles.boxTexto2, paddingRight:10,}}>
+                <View style={styles.spot} />
+                <Text style={styles.texto2}>
+                  Você pode ter confiança em nossa capacidade de adaptação às mudanças e de se manter na vanguarda de nosso setor, graças à nossa origem na academia e ao compromisso com a pesquisa e o desenvolvimento.
+                </Text>
+              </View>
+              <View style={styles.boxTexto2}>
+                <View style={styles.spot} />
+                <Text style={styles.texto2}>
+                  Nossa história como uma empresa fundada por estudantes também
+                  nos torna profundamente comprometidos em apoiar a próxima
+                  geração de empreendedores e inovadores.
+                </Text>
+              </View>
+            </View>
+          ) : (
+            console.log('HI')
+          )}
+        </View>
+
+        <View style={styles.spaceBetweenBoxes}>
+          <View style={styles.boxes}>
+            <Text style={styles.txt}>Quais são os benéficios do Stein</Text>
+            <TouchableOpacity style={styles}
+            onPress={()=>{setTexto3(!texto3)}}
+            >
+              <Image
+                source={{
+                  uri: 'https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48',
+                }}
+                style={{
+                    ...styles.imagem,
+                    transform: [{rotate: `${90 + (texto3 ? 180 : 0)}deg`}],
+                  }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.lines} />
+
+          {texto3 ? (
+            <View style={styles.boxTexto1}>
+              <View style={styles.boxTexto2}>
+                <View style={styles.spot} />
+                <Text style={styles.texto2}>
+                <Text style={styles.fontBold}>Ligue para nós:</Text> +55 (11) 2130-2615
+                </Text>
+              </View>
+              <View style={styles.boxTexto2}>
+                <View style={styles.spot} />
+                <Text style={styles.texto2}>
+                <Text style={styles.fontBold}>Nos envie um email:</Text>{"\n"}steinsuport@gmail.com
+                </Text>
+              </View>
+              <View style={{...styles.boxTexto2, paddingRight:10,}}>
+                <View style={styles.spot} />
+                <Text style={styles.texto2}>
+                <Text style={styles.fontBold}>Visite nossa sede:</Text>{"\n"}R. Sebastião Antonine Nº61,{"\n"}CEP 06622-180
+                </Text>
+              </View>
+            </View>
+          ) : (
+            console.log('HI')
+          )}
+
+        </View>
+
+        <View style={styles.spaceBetweenBoxes}>
+          <View style={styles.boxes}>
+            <Text style={styles.txt}>Como mudar minha senha</Text>
+            <TouchableOpacity style={styles}
+            onPress={()=>{setTexto4(!texto4)}}
+            >
+              <Image
+                source={{
+                  uri: 'https://firebasestorage.googleapis.com/v0/b/stein-182fa.appspot.com/o/Icons%2Fseta-direita.png?alt=media&token=c10be2a4-3f24-46f6-8368-7d40016bbe48',
+                }}
+                style={{
+                    ...styles.imagem,
+                    transform: [{rotate: `${90 + (texto4 ? 180 : 0)}deg`}],
+                  }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.lines} />
+          {texto4 ? (
+            <View style={styles.boxTexto1}>
+              <View style={styles.boxTexto2}>
+                <Text style={styles.texto2}>
+                <Text style={styles.fontBold}>1°. </Text>Abra o <Text style={[styles.fontBold, {color:"#563595"}]}>Stein</Text>. Talvez você precise fazer login.
+                </Text>
+              </View>
+              <View style={styles.boxTexto2}>
+              <Text style={styles.texto2}>
+                <Text style={styles.fontBold}>2°. </Text>Em "configurações", selecione "configurações de conta" e depois “conteúdo pessoal”.
+                </Text>
+              </View>
+              <View style={{...styles.boxTexto2, paddingRight:10,}}>
+              <Text style={styles.texto2}>
+                <Text style={styles.fontBold}>3°. </Text>Selecione “senha” e “alterar senha”. Digite sua nova senha.
+                </Text>
+              </View>
+            </View>
+          ) : (
+            console.log('HI')
+          )}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
 
 export default FaqScreen;
--- Stein/SteinProject/src/pages/FaqScreen/style.js
+++ Stein/SteinProject/src/pages/FaqScreen/style.js
@@ -1,5 +1,31 @@
 import {StyleSheet} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 import { verticalScale, scale, ScaledSheet, moderateScale, moderateVerticalScale } from "react-native-size-matters";
+
+const boxTexto = {
+    width:"100%",
+    padding:20,
+    backgroundColor:"rgba(255,0,183, 0.1)",
+    marginTop:10,
+    borderRadius: 10,
+}
+
+const texto = {
+    fontSize: RFValue(18),
+    textAlign:"justify",
+    color:"#000"
+}
+
+const spot = {
+    width:RFValue(10),
+    height:RFValue(10),
+    borderRadius:10,
+    backgroundColor:"#000",
+    marginTop:6,
+    marginRight:10,
+}
+
+const textos ={...texto, fontSize:RFValue(15)}
 
 const styles = ScaledSheet.create({
     container:{
@@ -15,10 +41,9 @@ const styles = ScaledSheet.create({
         marginBottom:20,
     },
     imagem:{
-        width:moderateScale(25),
-        height:moderateScale(25),
+        width:RFValue(25),
+        height:RFValue(25),
         resizeMode:"contain",
-        transform: [{rotate:'90deg'}],
     },
     boxes:{
         width:"100%",
@@ -27,7 +52,7 @@ const styles = ScaledSheet.create({
     },
     lines:{
         width:"100%",
-        height:2,
+        height:RFValue(2),
         backgroundColor:"#000000",
         marginTop:5,
     },
@@ -35,8 +60,33 @@ const styles = ScaledSheet.create({
         marginVertical:20,
     },
     txt:{
-        fontSize:moderateScale(16),
-    }
+        ...texto,
+        fontWeight:"700",
+        
+    },
+    boxTexto1:{
+        ...boxTexto,
+    },
+    boxTexto2:{
+        ...boxTexto,
+        flexDirection:"row",
+        alignItems:"flex-start",
+        backgroundColor:"rgba(0,0,0,0.0)",
+        padding:0,
+    },
+    texto1:{
+        ...textos,
+    },
+    texto2:{
+        ...textos,
+    },
+    spot:{
+        ...spot,
+    },
+    fontBold:{
+        fontWeight:"600",
+        fontSize:RFValue(16),
+    },
     
 })
 
